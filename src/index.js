46a77e1c55eb6c4cb3261dc2b7a97f7450bf55d7
Anton Eriksson <anton.eriksson@umu.se>
2019-09-27 10:35:21 +0200

    Add Sentry crash reporting

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+import * as Sentry from '@sentry/browser';
 import React from 'react';
 import { hydrate, render } from "react-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+Sentry.init({
+  dsn: "https://2527f14880aa45c8acbac7a5acffe71e@sentry.io/1763436",
+  beforeSend(event) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({
+        eventId: event.event_id,
+        subtitle2: "If you'd like to help, tell us what happened below. Your network data stay on your computer."
+      });
+    }
+    return event;
+  }
+});
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
