319d9ee186e1b837d1645b855ccc70ac68d0c659
Anton Eriksson <anton.eriksson@umu.se>
2020-03-05 13:37:08 +0100

    feat: Put the header images in the Steps

--- src/components/Infomap/Steps.js
+++ src/components/Infomap/Steps.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { Step } from "semantic-ui-react";
+import { Step, Image } from "semantic-ui-react";
+import './Steps.css';
+import Step1 from "../../images/step1.png";
+import Step2 from "../../images/step2.png";
+import Step3 from "../../images/step3.png";
 
 export default (props) => {
   const {
@@ -9,8 +13,9 @@ export default (props) => {
   } = props;
 
   return (
-    <Step.Group ordered>
+    <Step.Group>
       <Step completed={firstCompleted} active={firstActive}>
+        <Image size="tiny" src={Step1}/>
         <Step.Content>
           <Step.Title>Load network</Step.Title>
           <Step.Description>
@@ -20,6 +25,7 @@ export default (props) => {
       </Step>
 
       <Step completed={secondCompleted} active={secondActive}>
+        <Image size="tiny" src={Step2}/>
         <Step.Content>
           <Step.Title>Run Infomap</Step.Title>
           <Step.Description>
@@ -29,6 +35,7 @@ export default (props) => {
       </Step>
 
       <Step completed={thirdCompleted} active={thirdActive}>
+        <Image size="tiny" src={Step3}/>
         <Step.Content>
           <Step.Title>Explore map!</Step.Title>
           <Step.Description>
