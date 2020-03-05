31fb7596eb14d6de70f988290519088260740611
Anton Eriksson <anton.eriksson@umu.se>
2020-03-05 12:06:51 +0100

    refactor: InfomapOnline cleanup and move params to bottom
    refactor: Don't remove options from param descriptions
    
    feat: Bottom justify Infomap fields

--- src/components/Infomap/NavigatorButton.js
+++ src/components/Infomap/NavigatorButton.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { Button, Image } from "semantic-ui-react";
 
-export default ({ href, disabled }) => (
+export default (props) => (
   <Button
-    disabled={disabled}
     as="a"
-    href={href}
     target="_blank"
     rel="noopener noreferrer"
     style={{ padding: 10, marginTop: 20 }}
+    {...props}
   >
     <div
       style={{
@@ -21,12 +20,12 @@ export default ({ href, disabled }) => (
         Explore in
       </div>
       <Image
-        size="tiny"
+        size="mini"
         src="https://www.mapequation.org/assets/img/twocolormapicon_whiteboarder.svg"
       />
       <div
         className="brand"
-        style={{ fontSize: 18, lineHeight: 1.1, marginTop: 5 }}
+        style={{ fontSize: 16, lineHeight: 1.1, marginTop: 5 }}
       >
         <span className="brand-infomap">Infomap</span>{" "}
         <span className="brand-nn">Network Navigator</span>
