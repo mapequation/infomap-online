a9dd0b5cbdc92fa6d06ddb4a449d6bc1e2d42242
Anton Eriksson <anton.eriksson@umu.se>
2020-03-06 12:50:15 +0100

    refactor: Add Code.css

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Label, Segment } from "semantic-ui-react";
+import "./Code.css";
 
 export default ({ children, label }) => (
   <Segment as="pre" style={{ boxShadow: 'none' }}>
