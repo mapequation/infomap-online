9c81ed7ddca7638ecafbe10afccc3e655ca5b993
Anton Eriksson <anton.eriksson@umu.se>
2020-05-28 10:26:08 +0200

    fix: Don't show report dialog for NotFoundError
    This is caused by removed components inside fragments.
    
    Fixes #INFOMAP-ONLINE-R

--- src/index.js
+++ src/index.js
@@ -12,9 +12,14 @@ Sentry.init({
     process.env.NODE_ENV === "production"
       ? "https://2527f14880aa45c8acbac7a5acffe71e@sentry.io/1763436"
       : undefined,
-  beforeSend(event) {
+  beforeSend(event, hint) {
+    const error = hint.originalException;
+    // Dont show report dialog for React NotFoundErrors:
+    // NotFoundError: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.
+    const hasNotFoundError = error && error.message && error.message.match(/NotFoundError/i);
+
     // Check if it is an exception, and if so, show the report dialog
-    if (event.exception) {
+    if (event.exception && !hasNotFoundError) {
       Sentry.showReportDialog({
         eventId: event.event_id,
         subtitle2:
