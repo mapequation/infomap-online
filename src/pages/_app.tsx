7dd312a334f8f33c79f1cca3d88d110c82b0a6ff
Anton Eriksson <anton.eriksson@umu.se>
2022-02-22 16:04:40 +0100

    fix: Ignore color mode set by Alluvial or Bioregions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, GlobalStyle, LightMode } from "@chakra-ui/react";
 import theme from "../theme";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <LightMode>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </LightMode>
     </ChakraProvider>
   );
 }
