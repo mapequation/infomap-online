5f09e26774ebada0e080cf3167d66eab5f8c3280
Anton Holmgren <anton.holmgren@umu.se>
2023-05-29 10:01:51 +0200

    docs(examples): Add modular_w and modular_wd

--- src/components/Infomap/ExamplesMenu.js
+++ src/components/Infomap/ExamplesMenu.js
@@ -18,41 +18,91 @@ export default observer(function ExamplesMenu({ disabled }) {
       name: "twoTriangles",
       label: "Two triangles",
       value: exampleNetworks.twoTriangles,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
     },
     {
       name: "nineTriangles",
       label: "Nine triangles",
       value: exampleNetworks.nineTriangles,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
     },
     {
       name: "bipartite",
       label: "Bipartite",
       value: exampleNetworks.bipartite,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
     },
     {
       name: "multilayerIntra",
       label: "Multilayer with intra-layer links",
       value: exampleNetworks.multilayerIntra,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
     },
     {
       name: "multilayerIntraInter",
       label: "Multilayer with intra and inter-layer links",
       value: exampleNetworks.multilayerIntraInter,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
     },
     {
       name: "multilayer",
       label: "Full multilayer",
       value: exampleNetworks.multilayer,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
     },
     {
       name: "states",
       label: "State network representation of multilayer network",
       value: exampleNetworks.states,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
     },
     {
       name: "karate",
       label: "Zachary's karate club",
       value: exampleNetworks.karate,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
+    },
+    {
+      name: "modular_w",
+      label: "Modular network with weighted links",
+      value: exampleNetworks.modular_w,
+      args: {
+        "--directed": false,
+        "--two-level": true,
+      },
+    },
+    {
+      name: "modular_wd",
+      label: "Modular network with weighted directed links",
+      value: exampleNetworks.modular_wd,
+      args: {
+        "--directed": true,
+        "--two-level": true,
+      },
     },
   ];
 
@@ -70,7 +120,15 @@ export default observer(function ExamplesMenu({ disabled }) {
         {networks.map((network) => (
           <MenuItem
             key={network.name}
-            onClick={() => store.setNetwork(network)}
+            onClick={() => {
+              store.setNetwork(network);
+              Object.entries(network.args).forEach(([key, value]) => {
+                const p = store.params.getParam(key);
+                if (p.value !== value) {
+                  store.params.toggle(p);
+                }
+              });
+            }}
           >
             {network.label}
           </MenuItem>
